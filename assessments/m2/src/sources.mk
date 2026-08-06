d30eab44186bdcab4d4c917a395c0af590103bd8
xuanuyenvu <xuanuyenvu22@gmail.com>
2026-08-06 15:15:21 +0200

    module 2

--- assessments/m2/src/sources.mk
+++ assessments/m2/src/sources.mk
@@ -9,9 +9,27 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
-
-# Add your include paths to this variable
-INCLUDES = 
-
+ifeq ($(PLATFORM),MSP432)
+ 
+SOURCES = \
+	main.c \
+	memory.c \
+	interrupts_msp432p401r_gcc.c \
+	startup_msp432p401r_gcc.c \
+	system_msp432p401r.c
+ 
+INCLUDES = \
+	-Iinclude/common \
+	-Iinclude/msp432 \
+	-Iinclude/CMSIS
+ 
+else
+ 
+SOURCES = \
+	main.c \
+	memory.c
+ 
+INCLUDES = \
+	-Iinclude/common
+ 
+endif
